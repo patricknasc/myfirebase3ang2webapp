7483d88e976da6da7fec6e1e0e984069303c94dd
patricknasc <patrick.nascimento@gmail.com>
2017-03-23 22:40:11 -0300

    Firebase script tag deleted, and install via npm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initializeApp, database } from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(){
+
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyChfbiTaN7deX4Dyiggzv3vbq4LHqU50kw",
+      authDomain: "fir-crud-f765e.firebaseapp.com",
+      databaseURL: "https://fir-crud-f765e.firebaseio.com",
+      storageBucket: "fir-crud-f765e.appspot.com",
+      messagingSenderId: "450788208288"
+    };
+    initializeApp(config);
+
+     var root = database().ref();
+     root.on('value', function(snap){
+       console.log(snap.val());
+     })
+
+  }
 }
